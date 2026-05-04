1301ebd7c06e28556b362814655d9cd4682587f8
Galandar80 <calisma@gmail.com>
2026-05-04 15:08:05 +0200

    Update WhatsApp invite link

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <h4>Social</h4>
                     <div class="social-links">
                         <a href="https://discord.gg/YHej49XbHn" target="_blank" rel="noopener noreferrer" style="margin-right: 15px;"><i class="fab fa-discord"></i> Discord</a><br>
-                        <a href="https://chat.whatsapp.com/Ey0ypo4rohjB9vQMJSyJOG" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp"></i> WhatsApp</a>
+                        <a href="https://chat.whatsapp.com/HcZGGvsrMIKDpEnWZot6L2" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp"></i> WhatsApp</a>
                     </div>
                 </div>
             </div>
